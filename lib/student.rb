e83f026e33308acde3f6c7bbdf5b60875c59f9f2
Hayden Betts <haydenbetts@gmail.com>
2018-07-10 20:52:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,9 +64,10 @@ class Student
 
   def self.find_by_name(name)
     sql = <<-SQL
-      UPDATE students
-      SET name = ?, grade = ?
-      WHERE id = ?;
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1;
     SQL
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
